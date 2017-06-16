dd26c9b9067780a8c08f81373813a3622a8f4fca
Bill Peregoy <bill.peregoy@endurance.com>
2017-06-15 20:33:54 -0400

    Changed another function name and removed anonymous function

--- src/Demo.elm
+++ src/Demo.elm
@@ -47,11 +47,11 @@ type Msg
 mapOnFieldMatch : FieldName -> FieldSelection -> List Field -> List Field
 mapOnFieldMatch fieldName value fields =
     fields
-        |> List.map (\field -> conditionalFieldTransform fieldName value field)
+        |> List.map (updateOnMatch fieldName value)
 
 
-conditionalFieldTransform : FieldName -> FieldSelection -> Field -> Field
-conditionalFieldTransform fieldName value field =
+updateOnMatch : FieldName -> FieldSelection -> Field -> Field
+updateOnMatch fieldName value field =
     if field.fieldName == fieldName then
         { field | selectionStatus = value }
     else
