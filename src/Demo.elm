ec1a48f9056a5d8859b7daff3aa21d4e0d182018
Bill Peregoy <bill.peregoy@endurance.com>
2017-06-15 20:24:52 -0400

    Made update cases statements into one line expressions

--- src/Demo.elm
+++ src/Demo.elm
@@ -62,29 +62,13 @@ update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
         SetSelected fieldName True ->
-            { model
-                | contactFields =
-                    transformOneField fieldName (Selected Optional) model.contactFields
-            }
-                ! []
+            { model | contactFields = transformOneField fieldName (Selected Optional) model.contactFields } ! []
 
         SetSelected fieldName False ->
-            { model
-                | contactFields =
-                    transformOneField fieldName Unselected model.contactFields
-            }
-                ! []
+            { model | contactFields = transformOneField fieldName Unselected model.contactFields } ! []
 
         SetRequired fieldName True ->
-            { model
-                | contactFields =
-                    transformOneField fieldName (Selected Required) model.contactFields
-            }
-                ! []
+            { model | contactFields = transformOneField fieldName (Selected Required) model.contactFields } ! []
 
         SetRequired fieldName False ->
-            { model
-                | contactFields =
-                    transformOneField fieldName (Selected Optional) model.contactFields
-            }
-                ! []
+            { model | contactFields = transformOneField fieldName (Selected Optional) model.contactFields } ! []
