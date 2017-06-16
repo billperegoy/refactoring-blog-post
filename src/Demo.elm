d28df1d9937001d1328b418547251da662c75c10
Bill Peregoy <bill.peregoy@endurance.com>
2017-06-15 20:41:06 -0400

    Stopped using pipeline operator for very simple function call

--- src/Demo.elm
+++ src/Demo.elm
@@ -46,8 +46,7 @@ type Msg
 
 mapOnFieldMatch : FieldName -> FieldSelection -> List Field -> List Field
 mapOnFieldMatch fieldName value fields =
-    fields
-        |> List.map (updateOnMatch fieldName value)
+    List.map (updateOnMatch fieldName value) fields
 
 
 updateOnMatch : FieldName -> FieldSelection -> Field -> Field
